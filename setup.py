3a9827bfcd63cf14cfca027ae90a672a06e19674
AlexanderC <alexander.moldova@gmail.com>
2017-11-22 18:44:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     install_requires=[
         'requests>=2.0,<3.0',
         'Flask-Login>=0.2,<0.3',
+        'requests-toolbelt>=0.8.0',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
